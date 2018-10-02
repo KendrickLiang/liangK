b40b30b3d5b1b38dcc52a7e670a69d41006011b1
KendrickLiang <kliang5@stuy.edu>
2018-10-02 13:53:35 -0400

    add

--- 13_formation/form.py
+++ 13_formation/form.py
@@ -4,13 +4,9 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('form.html')
-@app.route('/auth')
+@app.route('/auth', method = ["GET", "POST"])
 def authenticate():
-    print(app)
-    print(request)
-    print(request.args)
     print(request.args['username'])
-    print(request.headers)
     return render_template('entered.html')
 if __name__ == "__main__":
     app.debug = True
