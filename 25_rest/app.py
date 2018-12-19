b55cf2c73e079ae93bb135b99fcbc112e4581a64
KendrickLiang <kliang5@stuy.edu>
2018-12-19 13:13:58 -0500

    add

--- 25_rest/app.py
+++ 25_rest/app.py
@@ -6,12 +6,11 @@ import json
 app = Flask(__name__)
 @app.route('/')
 def home():
-    enter = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=fYy5NFLdYdpo4Vp8MjgWUHVZdXzka19nk6nIAPKB&date=2018-04-15")
+    enter = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=BM40zJZKfQRSolaD95desU5DhA0YbS8m73dMzqUf&date=2018-11-15")
     display = enter.read()
     print (display)
     data=json.loads(display)
-    PIX_KEY = "10698567-19efb24bc53e467c50ad5a2ff"
-    PIX_URL = "https://pixabay.com/api/?key=" + PIX_KEY + "q=yellow+flowers&image_type=photo&pretty=true"
+    PIX_URL = "https://pixabay.com/api/?key=" + "10698567-19efb24bc53e467c50ad5a2ff" + "&q=yellow+flowers&image_type=photo&pretty=true"
     r = urllib.request.urlopen(PIX_URL)
     content = r.read()
     print("content:")
@@ -20,7 +19,7 @@ def home():
     pix_data = pix_data['hits']
     print("pix_data:")
     print(pix_data)
-    return render_template("index.html", pic=data['url'], explanation = data['explanation'], name=pix_data[0]['user'], pic2=pix_data[0]['largeImageURL'], info=pix_data[0]['tags'])
+    return render_template("index.html", pic=data['url'], description=data['explanation'], name=pix_data[0]['user'], pic2=pix_data[0]['largeImageURL'], info=pix_data[0]['tags'])
 if (__name__ == "__main__"):
     app.debug = True
     app.run()
